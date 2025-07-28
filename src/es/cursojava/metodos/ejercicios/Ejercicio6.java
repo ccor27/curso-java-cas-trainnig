d1297641f7ed71274e050bdea0a98f3d81088ced
ccor27_curso_java_cas <Tardes@GET-14>
2025-07-28 20:04:46 +0200

    added: cmabios

--- src/es/cursojava/metodos/ejercicios/Ejercicio6.java
+++ src/es/cursojava/metodos/ejercicios/Ejercicio6.java
@@ -34,9 +34,8 @@ public class Ejercicio6 {
 		// inicializar vidas
 		int attemps = 7;
 		System.out.println(userName + " vamos a jugar ahorcado, buena suerte!");
-		boolean gameFlag = true;
 		// un ciclo mientras tenga vida o encuentre la frase
-		while (gameFlag && attemps > 0) {
+		while (attemps > 0) {
 			System.out.println("Tienes " + attemps + " vidas");
 			printPhraseHangMan(phrase, letters);
 			character = Utilidades.pideDatoCadena("Ingresa una letra:");
@@ -44,15 +43,25 @@ public class Ejercicio6 {
 			if (!phrase.contains(character)) {
 				attemps--;
 			}
+			//comentario
 			if (wasPhraseFound(phrase, letters)) {
 				System.out.println("Felicidades " + userName + " has ganado");
-				gameFlag = false;
+				break;
 			}
+			showLetters(letters);
 		}
 		if(attemps == 0) {
 			System.out.println("Que pena "+userName+", has perdido!");
 		}
 
+	}
+	
+	public static void showLetters(String letters) {
+		System.out.print("\nLas letras que has introducido son: ");
+		for (int i = 0; i < letters.length(); i++) {
+			System.out.print(letters.charAt(i)+" ");
+		}
+		System.out.println();
 	}
 
 	public static boolean wasPhraseFound(String phrase, String letters) {
