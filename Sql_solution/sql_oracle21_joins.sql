d45dc714bdca5c1006958c15cb02d3b8281376ca
cristian <cristian@gmail.com>
2025-10-31 19:16:00 +0100

    sql

--- Sql_solution/sql_oracle21_joins.sql
+++ Sql_solution/sql_oracle21_joins.sql
@@ -48,22 +48,30 @@ GROUP BY
     r.region_name,
     d.department_name
 ORDER BY d.department_name;
------------ salario que se paga a esos empleados
+
+--- Mostrar el nombre completo de los empleados junto con el del departamento donde trabajan.
+--- Los empleados que no estén asignados, mostrarán el texto “Sin asignar”.
+--- Los departamentos sin empleados, mostrarán el texto “Sin empleados”.
+--- Ordenar los resultados por el nombre del departamento y el apellido del empleado
+
 SELECT
-    r.region_name,
-    d.department_name,
-    count (*) as CANTIDAD_EMPLEADOS,--agregacion, cada vez que se use esto, hay que hacer un group by
-    TO_CHAR(SUM(e.salary *( 1+ NVL(e.commission_pct,0))),'999G999D99L') as SALARIO_TOTAL
+    NVL(TRIM(e.first_name || ' ' || e.last_name), 'Sin empleados') AS NOMBRE_EMPLEADO,
+    NVL(d.department_name,'Sin asignar') AS DEPARTAMENTO
 FROM
-        regions r
-    inner join countries c on r.region_id = c.region_id
-    inner join locations l on c.country_id = l.country_id
-    inner join departments d on l.location_id = d.location_id
-    inner join employees e on d.department_id = e.department_id
-WHERE
-    r.region_name = 'Europe'
-GROUP BY
-    r.region_name,
-    d.department_name
-ORDER BY d.department_name;
+       departments d
+     FULL OUTER JOIN employees e on d.department_id = e.department_id
+ORDER BY DEPARTAMENTO , e.last_name DESC;
+
+-------------------------------------------------------------------------------------
+SELECT
+    first_name || ' ' || last_name AS empleado,
+    hire_date,
+    CASE
+        WHEN TO_DATE(TO_CHAR(SYSDATE, 'YYYY') || TO_CHAR(hire_date, '/MM/DD'), 'YYYY/MM/DD') < SYSDATE
+        THEN ADD_MONTHS(TO_DATE(TO_CHAR(SYSDATE, 'YYYY') || TO_CHAR(hire_date, '/MM/DD'), 'YYYY/MM/DD'), 12)
+        ELSE TO_DATE(TO_CHAR(SYSDATE, 'YYYY') || TO_CHAR(hire_date, '/MM/DD'), 'YYYY/MM/DD')
+    END AS proximo_cumple
+FROM employees;
+
+
     
